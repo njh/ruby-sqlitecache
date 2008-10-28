4772e1e4dad18c6aecb1451250ceff110d077a36
Nicholas J Humfrey <njh@aelius.com>
2008-10-28 17:43:44 +0000

    Turned the table name into a constant. Remove dependancy on 'CREATE TABLE IF NOT EXISTS'.

--- lib/sqlite_cache.rb
+++ lib/sqlite_cache.rb
@@ -8,37 +8,43 @@ require 'yaml'
 # Generic query cache, to store queries and their responses in an SQLite database
 class SqliteCache
 
+  TABLE_NAME = 'query_cache'
+
   # Create a new SQLiteCache 
   def initialize( path )
     @db = SQLite3::Database.new( path )
-    @db.busy_timeout(500)
-    @db.execute( %q[
-      CREATE TABLE IF NOT EXISTS query_cache (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        query TEXT,
-        response TEXT,
-        hits INTEGER DEFAULT 0,
-        created_at INTEGER
-      );
-    ] )
+    @db.busy_timeout(1000)   # Wait 1sec before failing to gain lock
+    
+    # Create the table, if it doesn't exist
+    if @db.table_info(TABLE_NAME).empty?
+      @db.execute( %Q{
+        CREATE TABLE #{TABLE_NAME} (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          query TEXT,
+          response TEXT,
+          hits INTEGER DEFAULT 0,
+          created_at INTEGER
+        );
+      } )
+    end
   end
   
   # Delete everything in the cache
   def purge
-    @db.execute( "DELETE FROM query_cache;" )
+    @db.execute( "DELETE FROM #{TABLE_NAME};" )
   end
   
   # Return the number of items in the cache
   def size
-    @db.get_first_value( "SELECT COUNT(*) FROM query_cache;" ).to_i
+    @db.get_first_value( "SELECT COUNT(*) FROM #{TABLE_NAME};" ).to_i
   end
 
   # Fetch something from the cache, based on a query object
   def fetch( query )
     id,response,hits = @db.get_first_row(
       "SELECT id,response,hits "+
-      "FROM query_cache "+
-      "WHERE query=?", 
+      "FROM #{TABLE_NAME} "+
+      "WHERE query=?",
       query.to_yaml( :SortKeys => true )
     )
 
@@ -46,7 +52,7 @@ class SqliteCache
     return nil if response.nil?
     
     # Increment the number of hits
-    @db.execute("UPDATE query_cache SET hits=? WHERE id=?", hits.to_i+1, id)
+    @db.execute("UPDATE #{TABLE_NAME} SET hits=? WHERE id=?", hits.to_i+1, id)
 
     # Otherwise if there is a HIT, parse the YAML into an object
     return YAML::load(response)
@@ -54,9 +60,8 @@ class SqliteCache
 
   # Store a query and response in the cache
   def store( query, response )
-    @db.execute(
-      %q{
-        INSERT INTO query_cache
+    @db.execute( %Q{
+        INSERT INTO #{TABLE_NAME}
         (query,response,created_at)
         VALUES (?,?,?)
       },
